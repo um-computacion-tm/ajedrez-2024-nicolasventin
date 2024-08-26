ea884fb9c0cbd9ef84f8e2929de57e580b17ae6f
Nico <nve2004@gmail.com>
2024-08-26 18:34:08 -0300

    pawn movements

--- game/pawn.py
+++ game/pawn.py
@@ -2,4 +2,20 @@ from piece import Piece
 
 
 class Pawn(Piece):
-    ...
+    def __str__(self):
+        if self.__color__ == "WHITE":
+            return "♟"
+        else:
+            return "♙"
+
+def pawn_move(self, from_row, from_col, to_row, to_col):
+    if self.__color__ == "White":
+         if from_col == to_col and (from_row - to_row == 1 or (from_row == 6 and from_row - to_row == 2)):
+            return True
+         else:
+            if from_col == to_col and (to_row - from_row == 1 or (from_row == 1 and to_row - from_row == 2)):
+                return True
+    return False
+
+
+# falta agregar la logica de comer las piezas en diagonal
