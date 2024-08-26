8d74c77e6936bfa1f85314344fcdbab9e5cd59b2
Nico <nve2004@gmail.com>
2024-08-26 20:59:24 -0300

    Tests for king movements

---
+++ tests/test_king.py
@@ -0,0 +1,18 @@
+import unittest
+from game.king import King
+
+class TestKing(unittest.TestCase):
+    
+    def setUp(self):
+        self.white_king = King("White")
+        self.black_king = King("Black")
+
+    def test_king_valid_moves(self):
+        self.assertTrue(self.white_king.king_move(4, 4, 5, 5))  # Diagonal
+        self.assertTrue(self.white_king.king_move(4, 4, 4, 5))  # Derecha
+        self.assertTrue(self.white_king.king_move(4, 4, 5, 4))  # Abajo
+        self.assertTrue(self.white_king.king_move(4, 4, 3, 3))  # Diagonal inversa
+        self.assertTrue(self.white_king.king_move(4, 4, 4, 3))  # Izquierda
+
+    def test_king_invalid_moves(self):
+        self.assertFalse(self.white_king.king_move(4, 4, 6, 6))  # Fuera de rango
