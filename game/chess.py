354dc19b04ad0ef198e590f1f3bbc981539ff2af
Nico <nve2004@gmail.com>
2024-08-26 23:00:16 -0300

    Se añadieron validaciones y se actualizo el tablero

--- game/chess.py
+++ game/chess.py
@@ -1,6 +1,5 @@
 from board import Board
 
-
 class Chess:
     def __init__(self):
         self.__board__ = Board()
@@ -9,16 +8,24 @@ class Chess:
     def is_playing(self):
         return True
 
-    def move(
-        self,
-        from_row,
-        from_col,
-        to_row,
-        to_col,
-    ):
-        # validate coords
+    def move(self, from_row, from_col, to_row, to_col):
+
         piece = self.__board__.get_piece(from_row, from_col)
+
+        if not piece:
+            raise ValueError("No hay ninguna pieza en la posición inicial")
+        
+        if piece.color != self.__turn__:
+            raise ValueError(f"Es el turno de {self.__turn__})")
+
+        # verifica si el rey puede realizar ese movimiento
+        if not piece.king_move(from_row, from_col, to_row, to_col):
+            raise ValueError("Movimiento inválido para la pieza")
+
+        self.__board__.move_piece(from_row, from_col, to_row, to_col)
+
         self.change_turn()
+
     @property
     def turn(self):
         return self.__turn__
@@ -31,4 +38,5 @@ class Chess:
             self.__turn__ = "BLACK"
         else:
             self.__turn__ = "WHITE"
+
 
